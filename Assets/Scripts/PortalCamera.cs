526c59c96036cf40da268d36f34d577340207b4e
Alec <Quaintman2000@gmail.com>
2021-12-16 03:17:52 -0700

    finished the game

---
+++ Assets/Scripts/PortalCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera;
+    [SerializeField] Transform portal;
+    [SerializeField] Transform otherPortal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        playerOffsetFromPortal = Vector3.ClampMagnitude(playerOffsetFromPortal, 3);
+        transform.position = portal.position + playerOffsetFromPortal;
+
+
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.localRotation, otherPortal.localRotation);
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+}
---
+++ Assets/Scripts/PortalTextureSetup.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    [SerializeField] Camera cameraGrey;
+
+    [SerializeField] Material cameraMatGrey;
+
+    //[SerializeField] Camera cameraA;
+
+    //[SerializeField] Material cameraMatA;
+
+    [SerializeField] Camera[] goodCameras;
+    [SerializeField] Material[] goodCameraMats;
+
+    [SerializeField] Camera[] badCameras;
+    [SerializeField] Material[] badCameraMats;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < goodCameras.Length; i ++)
+        {
+            if (goodCameras[i].targetTexture != null)
+            {
+                goodCameras[i].targetTexture.Release();
+            }
+            goodCameras[i].targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+            goodCameraMats[i].mainTexture = goodCameras[i].targetTexture;
+        }
+
+        //if (cameraA.targetTexture != null)
+        //{
+        //    cameraA.targetTexture.Release();
+        //}
+        //cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        //cameraMatA.mainTexture = cameraA.targetTexture;
+
+        //if (cameraB.targetTexture != null)
+        //{
+        //    cameraB.targetTexture.Release();
+        //}
+        //cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        //cameraMatB.mainTexture = cameraB.targetTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
